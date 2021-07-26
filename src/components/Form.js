c6c27d44a8c896d21eb5db2b6b6205564372e9d6
Blaire <grant.blaire@gmail.com>
2021-07-26 15:32:07 -0500

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,81 @@
 import React from 'react';
 
-class Form extends React.Component {
-  state = {
-    firstName: "John",
-    lastName: "Henry"
+  class Form extends React.Component {
+    render() {
+      return (
+        <div>
+          <form>
+            <input
+              type="text"
+              name="firstName"
+              onChange={event => this.props.handleChange(event)}
+              value={this.props.formData.firstName}
+            />
+            <input
+              type="text"
+              name="lastName"
+              onChange={event => this.props.handleChange(event)}
+              value={this.props.formData.lastName}
+            />
+          </form>
+        </div>
+      )
+    }
   }
+  
+export default Form; 
 
-  handleFirstNameChange = event => {
-    this.setState({
-      firstName: event.target.value
-    })
-  }
+// import React from 'react';
 
-  handleLastNameChange = event => {
-    this.setState({
-      lastName: event.target.value
-    })
-  }
+// class Form extends React.Component {
+  // state = {
+  //   firstName: "John",
+  //   lastName: "Henry",
+  //   submittedData: []
+  // }
 
-  render() {
-    return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
-    )
-  }
-}
+  // handleFirstNameChange = event => {
+  //   console.log(event.target.value)
+  //   this.setState({
+  //     firstName: event.target.value
+  //   })
+  // }
 
-export default Form;
+  // handleLastNameChange = event => {
+  //   console.log(event.target.value)
+  //   this.setState({
+  //     lastName: event.target.value
+  //   })
+  // }
+
+  // handleSubmit = event => {
+  //   event.preventDefault()
+  //   let formData = {
+  //     firstName: this.state.firstName,
+  //     lastName: this.state.lastName
+  //   }
+  //   let dataArray = this.state.submittedData.concat(formData)
+  //   this.setState({submittedData: dataArray})
+  //   console.log(dataArray)
+  //   // this.sendFormDataSomewhere(formData)
+  // }
+
+  // listOfSubmissions = () => {
+  //   return this.state.submittedData.map(data => {
+  //     return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div>
+  //   })
+  // }
+
+  // render() {
+  //   return (
+  //     <div>
+  //       <form onSubmit={event => this.handleSubmit(event)}>
+  //         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
+  //         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+  //         <input type="submit"/>
+  //       </form>
+  //       {this.listOfSubmissions()}
+  //     </div>
+  //   )
+  // }
+  
